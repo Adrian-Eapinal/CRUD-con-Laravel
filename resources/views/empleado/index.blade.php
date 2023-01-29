85e5b85fd2fc9ec564002f37793a8d41bea1caeb
Adrian Espinal <lacomputer07@gmail.com>
2023-01-29 17:39:05 -0400

    Trabajando en el listado

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -1,1 +1,24 @@
 Mostrar la lista de empleados
+<table class="table table-light">
+
+    <thead class="thead-light">
+        <tr>
+         <th>#</th>
+         <th>Foto</th>
+         <th>Nombre</th>
+         <th>Apellido Paterno</th>
+         <th>Apellido Materno</th>
+         <th>Correo</th>
+         <th>Acciones</th>
+        </tr>
+    </thead>
+
+
+    <tbody>
+        @foreach(  )
+        <tr>
+            <td></td>
+        </tr>
+    </tbody>
+
+</table>
